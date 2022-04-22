e92d5bc8baefc13c48afa362bb0ba5dfe72a5f59
Kat Lane <grimm0kitten@gmail.com>
2022-04-21 20:53:54 -0400

    levl 8 kyu on strings

---
+++ Level 8 Kyu JavaScript kata/Remove First and Last Character.js
@@ -0,0 +1,17 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+
+// function removeChar(str){
+//     let finalString = str.slice(1, -1)
+//     return finalString
+   
+//    };
+   
+   
+   const removeChar = str => str.slice(1,-1);
+
+
+
+
+
+   console.log(removeChar("Hello"));
