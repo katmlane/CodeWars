bce3ef0587c86159444a15ff837f9cdd4d0465aa
Kat Lane <97710598+katmlane@users.noreply.github.com>
2022-05-03 21:24:20 -0400

    Create You Can't Code Under Pressure #1.js
    level 8 kata on being efficient with integers

---
+++ JavaScript/8 kyu/You Can't Code Under Pressure #1.js
@@ -0,0 +1,21 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i;
+}
+
+function doubleInteger(i) {
+  i = i * 2;
+  return i;
+}
+
+console.log(doubleInteger(5));
+
+let doubleInteger = (i) => 2 * i;
+
+//this one was pretty clever
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
