204d9a08430a473c785046e4085daadb53fe06ba
Kat Lane <katmlanedesigns@gmail.com>
2022-06-19 18:33:13 -0400

    daily codewar 6.18.22

---
+++ JavaScript/8 kyu/is the string uppercase.js
@@ -0,0 +1,58 @@
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+
+// String.prototype.isUpperCase = function() {
+//     // your code here
+//   }
+
+//parameters
+
+//string 
+
+//return
+
+//boolean
+//true if all caps
+//false if not
+
+//examples
+
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+
+//pseudocode
+
+//need to split into an array 
+//need to loop through array and see if .toUpperCase is true
+//if true return true
+
+const isUpperCase = str => {
+
+    const strArr = str.split(" ").join("").split("");
+    //console.log(strArr)
+    let lowerCaseArr = 0;
+     strArr.map((letter) => {
+        if (letter !== letter.toUpperCase()) {
+            lowerCaseArr++
+        }
+    })
+    console.log(lowerCaseArr)
+
+    return lowerCaseArr > 0 ? false : true;
+}
+
+console.log(isUpperCase("ACSKLDFJSGSKLDFJSKLDFJ"))
