6f866fa6dfa696afa58ba549b415895fcf3545a1
Kat Lane <KatMLaneDesigns@gmail.com>
2022-07-06 06:34:39 -0400

    Create squaring-an-argument.js
    simple kata using Math.pow() to square an integer

---
+++ JavaScript/8 kyu/squaring-an-argument.js
@@ -0,0 +1,25 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+
+//parameters
+
+//integer
+
+//return
+
+// square of integer
+
+//example 
+
+//console.log(square(3))  //9
+
+//pseudocode
+
+//I will be using the Math.pow to complete this kata
+
+
+const square = n => Math.pow(n , 2);
+
+
+console.log(square(3));
