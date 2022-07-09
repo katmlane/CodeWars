cfdbb81062e7a5abad5240df68ee44fbdcf42fd0
Kat Lane <katmlanedesigns@gmail.com>
2022-07-08 23:03:13 -0400

    simple kata on mathmatics

---
+++ JavaScript/8 kyu/transportation-on-vacation.js
@@ -0,0 +1,59 @@
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+
+// function rentalCarCost(d) {
+//     // Your solution here
+//   }
+
+//parameters
+
+//integer of days
+
+//return 
+
+//return integer of the amount of money it will cost
+
+//examples
+
+//console.log(rentalCarCost(1)) // 40
+//console.log(rentalCarCost(6)) // 220
+//console.log(rentalCarCost(9)) // 310
+//console.log(rentalCarCost(7)) // 230
+//console.log(rentalCarCost(4)) // 140
+
+
+//pseudo code
+
+//should have a conditional for the 3 options 
+
+// baseRental = 40 * d
+
+// if (d > 6) return baseRental - 50
+
+//if (d > 2 && d < 7) return baseRental - 20
+
+
+const rentalCarCost = d => {
+    const baseRental = 40 * d;
+
+    if (d < 3) {
+        console.log("no discount")
+        return baseRental
+    }
+    else if (d > 2 && d < 7){
+        console.log("three day discount")
+        return baseRental - 20
+    }
+    else if (d > 6) {
+        console.log("seven day discount")
+        return baseRental - 50 
+    }
+};
+
+console.log(rentalCarCost())
