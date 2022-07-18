7a9c152083a28803245e229d7afcf16d77203739
Kat Lane <KatMLaneDesigns@gmail.com>
2022-07-18 06:02:52 -0400

    Create Removing-elements.js
    lvl 8 kata on removing elements

---
+++ JavaScript/8 kyu/Removing-elements.js
@@ -0,0 +1,42 @@
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+// function removeEveryOther(arr){
+//   //your code here
+// }
+
+//parameters 
+
+// an array of strings 
+
+//return 
+
+// an array with every other element removed
+
+//examples
+
+//console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again'])) //['Hello', 'Hello Again']
+//console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) //[1, 3, 5, 7, 9]
+//console.log(removeEveryOther([[1, 2]]), [[1, 2]])) //
+//console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}])) //[['Goodbye']]
+
+//pseudocode
+
+// need to remove every other element
+// going to use loop to push to new array
+
+const removeEveryOther = arr => {
+  let keepArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 !== 1) {
+      keepArr.push(arr[i])
+    }
+  }
+  return keepArr;
+}
+
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
