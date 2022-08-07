bfff1449d6cee9639a32b7fc071def8bbcaf4484
Kat Lane <katmlanedesigns@gmail.com>
2022-08-07 04:12:43 -0400

    daily codewars 8-7-22

---
+++ JavaScript/8 kyu/plural.js
@@ -0,0 +1,39 @@
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+// All values will be positive integers or floats, or zero.
+
+
+
+// function plural(n) {
+//   // ...
+// }
+
+
+//parameters
+
+//integer 
+
+//returns
+
+// true if > 1, false = 1
+
+//examples
+
+
+//console.log(plural(0)) //true
+//console.log(plural(0.5)) //true
+//console.log(plural(1)) // false
+//console.log(plural(100)) //true
+//console.log(plural(Infinity)) //true
+
+
+//pseudocode
+
+// need conditional to check if 1 or not
+
+  
+const plural = n => n === 1 ? false : true;
+
+console.log(plural(0.5))
