f8387757e36cf839eecfa113464d17a176fa3f8b
Kat Lane <katmlanedesigns@gmail.com>
2022-09-08 15:44:58 -0400

    daily kata 9.6.22

---
+++ JavaScript/7 kyu/sort-arrays-1.js
@@ -0,0 +1,22 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+//parameters
+
+//an array of words
+
+//return
+
+//an array sorted in lexicographical order. 
+
+//examples
+
+// console.log(sortme(['one', 'two', 'three' ])) \\ ["one", "three", "two"])
+//pseudocode
+
+//should just be able to use sort
+
+sortme = function( names ){
+    return names.sort()
+  }
