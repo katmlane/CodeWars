5ece58492685ce769546970f01a8721920da790a
Kat Lane <katmlanedesigns@gmail.com>
2022-11-07 02:58:41 -0500

    kata I struggled with due to math, needs work

---
+++ JavaScript/8 kyu/no-zeros-for-heros.js
@@ -0,0 +1,26 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+
+
+// function noBoringZeros(n) {
+//   // your code
+// }
+
+// need a while loop to keep dividing by zero until the number is not divisible by 10.
+
+
+const noBoringZeros = n => {
+    while(n%10==0 && n!=0){
+      n/=10;
+    }
+   return n;
+ }
