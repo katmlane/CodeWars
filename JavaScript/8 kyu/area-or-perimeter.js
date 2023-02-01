a95a84453581313eae3c4b876eb09473a1de9dd4
katmlane <katmlanedesigns@gmail.com>
2023-02-01 03:26:05 -0500

    renamed and removed arrow functions

--- JavaScript/8 kyu/area-or-perimeter.js
+++ JavaScript/8 kyu/area-or-perimeter.js
@@ -50,3 +50,13 @@ const areaOrPerimeter = (l , w) => l === w ? l*w : 2*l + 2*w;
 
 
 console.log(areaOrPerimeter(3,3))
+
+
+function areaOrPerimeter (l,w) {
+      if (l === w) {
+    return l*w
+  }
+  else {
+    return 2*l + 2*w 
+  }
+}
