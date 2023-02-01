5f34cdd41d44769654bf26b932d3837e9d083a3b
katmlane <katmlanedesigns@gmail.com>
2023-02-01 03:02:58 -0500

    updated code to ommit arrow function

--- JavaScript/7 kyu/isograms.js
+++ JavaScript/7 kyu/isograms.js
@@ -1,12 +1,12 @@
-An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
-Example: (Input --> Output)
+// Example: (Input --> Output)
 
-"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
 
-isIsogram "Dermatoglyphics" = true
-isIsogram "moose" = false
-isIsogram "aba" = false
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
 
 
 
@@ -30,3 +30,17 @@ console.log(isIsogram("Dermatoglyphics"))
 // isIsogram "Dermatoglyphics" = true
 
 // isIsogram "moose" = false
+
+
+function isIsogram (str) {str = str.toLowerCase();
+  
+  for (let i = 0; i < str.length - 1; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  
+  return true;
+}
