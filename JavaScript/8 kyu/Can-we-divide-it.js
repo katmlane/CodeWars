00fda508667d23cc447f9f641aafa5bc55e5d052
Kat Lane <KatMLaneDesigns@gmail.com>
2023-02-11 03:55:39 -0500

    Update and rename Can we divide it.js to Can-we-divide-it.js
    renamed and removed arrow function

--- JavaScript/8 kyu/Can-we-divide-it.js
+++ JavaScript/8 kyu/Can-we-divide-it.js
@@ -48,7 +48,9 @@
 
 const isDivideBy = (number, a , b) => number % a === 0 && number % b === 0 ? true : false;
 
-
+function isDivideBy (number, a, b) {
+return number % a === 0 && number % b === 0 ? true : false
+}
 console.log(isDivideBy(45, 1, 6))
 
 // after completing the kata I realized I was using a string and not a boolean in the return
