a69a703e6be47a8c29e2000af361038b7aebb785
Kat Lane <KatMLaneDesigns@gmail.com>
2023-02-16 22:56:56 -0500

    Update and rename Convert a string to an array.js to Convert-a-string-to-an-array.js
    name change and code omission

--- JavaScript/8 kyu/Convert-a-string-to-an-array.js
+++ JavaScript/8 kyu/Convert-a-string-to-an-array.js
@@ -21,21 +21,4 @@ function stringToArray (str) {
 
 
 
-//figure out how they solved it!
 
-function stringToArray(string){
-    var result = [];
-    var traverse = function foo(string){
-        for(var i=0;i<string.length;i++){
-            var chr = string.charAt(i);
-            if(chr === " "){
-                result.push(string.slice(0,i));
-                foo(string.slice(i+1));
-                return;
-            }
-        }
-        result.push(string);
-    };
-    traverse(string);
-    return result;
-}
